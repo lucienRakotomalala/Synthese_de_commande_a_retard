b1cfc449795dd7ff6b0bd54658539d5caa1210e8
DavidTocaven <David.tocaven@gmail.com>
2018-02-21 17:26:25 +0100

    presque fini

--- matlab/SpectreFini_script.m
+++ matlab/SpectreFini_script.m
@@ -4,7 +4,7 @@
 % Fichiers Contenant le correcteur par spectre fini
 Modeles
 %%
-vp_desirees = [ -1/8 , -1/3      ] ;
+vp_desirees = [ -1/8 , -1/3   ] ;
 K =place(m.ee(2).a,m.ee(2).b,vp_desirees);
 
 ex1 = expm(m.ee(2).a*h);
