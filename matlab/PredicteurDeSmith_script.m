0e1208dab2845f9ef307c23bccff08ae227e80fa
Lucien <lulu.rakoto@gmail.com>
2018-02-21 17:27:36 +0100

    Smith OK

--- matlab/PredicteurDeSmith_script.m
+++ matlab/PredicteurDeSmith_script.m
@@ -7,4 +7,20 @@
 SpectreFini_script
 
 %% Ajout Perturbation
-p0 = 0;
+p0 = 100;
+h = 5
+%% Etude des perturbations
+
+%% Affichage 
+save('VgCorSmith.mat','VgCorSmith')
+fig5=figure(5);
+    plot(VgCorSmith.time,[0 ;ones(length(VgCorSmith.time)-1,1)],... % ref
+         VgCorSmith.time,.95*ones(size(VgCorSmith.time)),... % 95% Pour le temps de réponse
+         VgCorSmith.time,.63*ones(size(VgCorSmith.time)),... % 63% pour tau => Temps de monté
+         VgCorSmith.time,VgCorSmith.signals.values)
+    grid on
+    legend('U_m(t)','95%','63%','Vg(t) BF')
+    title('Réponse à un échelon unité en BF avec un Préditeur de Smith')
+    axis([-.5 20 0 1.1])
+    %%
+    savefig('fig5.fig','fig5')
