091ecc29cbba745bb07dff39d848547c1d5a6976
Lucien <lulu.rakoto@gmail.com>
2018-02-21 12:28:37 +0100

    Schema Bloc pret, script pret.
    	Reste a faire : Fonction de transfert BF, Simulation

---
+++ matlab/PredicteurDeSmith_script.m
@@ -0,0 +1,10 @@
+% TP - Synthèse d'une commande à retard - Application à un procédé électro-mécanique
+% M2 - ISTR - Semestre 2
+% Lucien Rakotomalala - David Tocaven
+% Fichiers Contenant le correcteur avec predicteur de Smith
+
+%% Utilisation du correcteur de Spectre
+SpectreFini_script
+
+%% Ajout Perturbation
+p0 = 0;
